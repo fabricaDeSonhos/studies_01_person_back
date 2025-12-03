d2be20d14f1edd4b13cdfea963181bc030a8dc02
Hylson <hvescovi@gmail.com>
2025-12-02 21:44:52 -0300

    chore:separate model from test

--- model_person.py
+++ model_person.py
@@ -13,20 +13,3 @@ db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 
 db.generate_mapping(create_tables=True)
 
-#Criar uma pessoa:
-with db_session:
-    p = Pessoa(nome="Maria Silva", email="maria@example.com", telefone="11999998888")
-
-#Consultar:
-with db_session:
-    pessoas = Pessoa.select()[:]
-    print(pessoas)
-
-#Atualizar:
-with db_session:
-    pessoa = Pessoa[1]
-    pessoa.telefone = "11988887777"
-
-#Deletar:
-with db_session:
-    Pessoa[1].delete()
